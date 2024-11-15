97246df10f6e2c483604da3bca0ec2b1268408a6
zadnap <zadnap@users.noreply.github.com>
2024-11-15 22:23:15 +0700

    Create an initial condition
    If user does have data in their local storage,
    whenever they visits the app or refesh, fetch
    the latest data and update them into local storage
    
    Besides, I also added favicon for the app.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { handleSearch } from "./controllers/appController";
+import { locationInformation } from "./utils/storage";
+
+if (locationInformation.get()) {
+  handleSearch(locationInformation.get().address);
+}
