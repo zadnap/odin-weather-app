51a969151fc5101554f307e21c7e3198db758afd
zadnap <zadnap@users.noreply.github.com>
2024-11-16 10:31:38 +0700

    Remove SASS from the project
    I decided to use pure CSS for the sake of
    simplifying the project.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import "./assets/css/reset.css";
+import "./assets/css/variables.css";
 import "./assets/css/main.css";
 
 import { handleSearch } from "./controllers/appController";
 import { locationInformation } from "./utils/storage";
 
-if (locationInformation.get()) {
-  handleSearch(locationInformation.get().address);
-}
+document.addEventListener("DOMContentLoaded", () => {
+  if (locationInformation.get()) {
+    handleSearch(locationInformation.get().address);
+  }
+});
