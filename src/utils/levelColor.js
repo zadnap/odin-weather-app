3091bd3c8a0d6a75fedd8970493bbbd6a2c67db8
zadnap <zadnap@users.noreply.github.com>
2024-11-19 16:10:27 +0700

    Fix getLevelColor() function
    Change the basis value to 100 and change the level
    range:
    - Safe: 0 - below 30
    - Moderate: 30 - 70
    - Danger: above 70 - 100

--- src/utils/levelColor.js
+++ src/utils/levelColor.js
@@ -1,9 +1,9 @@
 function getLevelColor(level, max) {
-  const relativeLevel = level / max;
+  const relativeLevel = (level / max) * 100;
 
-  if (relativeLevel < 20) {
+  if (relativeLevel < 30) {
     return "safe";
-  } else if (relativeLevel >= 20 && relativeLevel <= 80) {
+  } else if (relativeLevel >= 30 && relativeLevel <= 70) {
     return "moderate";
   } else {
     return "danger";
